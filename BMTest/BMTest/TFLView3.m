75bb2163b172643a1356e4f4c2d17342d04ddeda
fginha599 <xkn9484815@163.com>
2021-05-08 11:30:59 +0800

    提交修改的1.0bug

--- BMTest/BMTest/TFLView3.m
+++ BMTest/BMTest/TFLView3.m
@@ -33,17 +33,19 @@
 }
 
 
+//现在修改version1中的不能读图片的bug
+-(void)fixReadPicBug
+{
+
+    NSLog(@"=====fixReadPicBug");
+
+
+}
+
 -(void)version1Formanager{
 
 
     NSLog(@"经理创建了版本1了");
-
-}
-
-
--(void)completion2ForManager{
-
-    NSLog(@"接着完成版本2后面的功能");
 
 }
 @end
