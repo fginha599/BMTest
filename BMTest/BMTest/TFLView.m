97ede9895bb1d5839804a8f36252768cc0c675db
fginha599 <xkn9484815@163.com>
2021-05-08 09:21:45 +0800

    测试

---
+++ BMTest/BMTest/TFLView.m
@@ -0,0 +1,21 @@
+//
+//  TFLView.m
+//  BMTest
+//
+//  Created by mac on 16/5/16.
+//  Copyright © 2016年 www.tang. All rights reserved.
+//
+
+#import "TFLView.h"
+
+@implementation TFLView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
